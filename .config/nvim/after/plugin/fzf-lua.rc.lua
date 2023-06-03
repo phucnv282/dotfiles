c05a7f0aeb590adf7adeb2da0f5e3d8f4b65fd65
phucnv282 <phucnv282@gmail.com>
2023-06-03 16:46:12 +0700

    update fzf-lua exclude

--- .config/nvim/after/plugin/fzf-lua.rc.lua
+++ .config/nvim/after/plugin/fzf-lua.rc.lua
@@ -2,8 +2,14 @@ local status, fzf = pcall(require, "fzf-lua")
 if (not status) then return end
 
 fzf.setup({
-  'fzf-native'
+  files = {
+    fd_opts = "--color=never --type f --hidden --follow --exclude .git --exclude node_modules --exclude .next",
+  },
+  grep = {
+    rg_opts =
+    "-g '!.git' -g '!node_modules' --column --line-number --no-heading --color=always --smart-case --max-columns=4096",
+  }
 })
 
 vim.keymap.set("n", ";f", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
-vim.keymap.set("n", ";r", "<cmd>lua require('fzf-lua').live_grep()<CR>", {})
+vim.keymap.set("n", ";r", "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
