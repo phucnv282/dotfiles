0a14b95a90468fd629b33febd882cc4fcf54dedd
phucnv282 <phucnv282@gmail.com>
2023-06-03 08:39:40 +0700

    [nvim]: add toggleterm

--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -3,7 +3,7 @@ if (not status) then return end
 
 saga.setup({
   ui = {
-    winblend = 10,
+    winblend = 0,
     border = 'rounded',
     colors = {
       normal_bg = '#002b36'
@@ -14,7 +14,6 @@ saga.setup({
   }
 })
 
-local diagnostic = require("lspsaga.diagnostic")
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
