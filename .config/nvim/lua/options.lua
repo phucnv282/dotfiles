d7bbdc4bab177b16bc5ca60e7264b0ed0838e059
phucnv282 <phucnv282@gmail.com>
2023-06-04 11:26:36 +0700

    disable swap

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -40,3 +40,4 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
+opt.swapfile = false
