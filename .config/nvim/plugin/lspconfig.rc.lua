caf307dba1f4c7b6452f9e4e4f84714c53a0bc66
phucnv282 <phucnv282@gmail.com>
2023-06-04 09:15:21 +0700

    add null-ls

--- .config/nvim/plugin/lspconfig.rc.lua
+++ .config/nvim/plugin/lspconfig.rc.lua
@@ -97,7 +97,7 @@ lspconfig["lua_ls"].setup({
 
 -- Change the Diagnostic symbols in the sign column (gutter)
 -- (not in youtube nvim video)
-local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = "󱩖 ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
