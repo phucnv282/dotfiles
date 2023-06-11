a57df1de6b10812e6f89e855b2e3002c0ba46d5f
Phúc Nguyễn <33126800+phucnv282@users.noreply.github.com>
2023-06-11 00:23:11 -0700

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -31,10 +31,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# kubectx + kubens
-kubectl krew install ctx
-kubectl krew install ns
-
 # symbolic link
 rm -rf ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
