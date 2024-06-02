7eaa423debab9b05ce5d0234e24918ce687563e0
phucnv282 <phucnv282@gmail.com>
2024-06-02 14:36:47 +0700

    nvim: update

--- .config/nvim/lua/phucnv/plugins/nvim-cmp.lua
+++ .config/nvim/lua/phucnv/plugins/nvim-cmp.lua
@@ -6,8 +6,6 @@ return {
     "hrsh7th/cmp-path", -- source for file system paths
     {
       "L3MON4D3/LuaSnip",
-      -- follow latest release.
-      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
       -- install jsregexp (optional!).
       build = "make install_jsregexp",
     },
